333aeece8c54a767ba2c3116a3ce6f1b6d6256a5
Jiji <Fahd.Jiji@servisa-gruppe.de>
2025-07-03 10:59:37 +0200

    Update requirements

---
+++ schemas.py
@@ -0,0 +1,21 @@
+﻿from fastapi_users import schemas
+
+class RoleRead(schemas.BaseModel):
+    id: uuid.UUID
+    name: str
+
+class UserCreate(schemas.UC):
+    # inherits email & password
+    pass
+
+class UserRead(schemas.UR):
+    id: uuid.UUID
+    email: str
+    is_active: bool
+    is_superuser: bool
+    roles: list[RoleRead]
+
+class UserUpdate(schemas.UU):
+    is_active: Optional[bool]
+    is_superuser: Optional[bool]
+    roles: Optional[list[uuid.UUID]]
